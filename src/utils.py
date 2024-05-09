94abc0b2007d195d9c98611687f289e9b91af8e9
Akhil Pothanapalli <akhil.pothanapalli@gmail.com>
2024-05-09 16:22:31 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,8 @@ import sys
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,5 +21,47 @@ def save_object(file_path, obj):
             pickle.dump(obj, file_obj)   # dump function will serialize the python object "obj"
                                         #serialization converts the object into byte stream and will be writeen into file_obj
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+
+    try:
+        report ={}
+
+        for i in range(len(list(models))): #looping through the models 
+            model = list(models.values())[i]     #selecting a model and its parameters 
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3) # hyper parameter tuning is done here
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            # train to score for best model selection
+            # test score to its rel performance
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
     except Exception as e:
         raise CustomException(e, sys)
